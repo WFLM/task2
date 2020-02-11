5800c0abdef578c929019bdfd343196fc77dc9e3
Yury Kuznetsov <yurykuznetsov@protonmail.com>
2020-02-11 21:48:42 +0300

    add main functionality

--- task2/task_2.py
+++ task2/task_2.py
@@ -1,6 +1,72 @@
+from functools import total_ordering
+import re
+
+
+RELEASE_CANDIDATE_WEIGHT = -1
+BETA_WEIGHT = -2
+ALPHA_WEIGHT = -3
+
+
+@total_ordering
 class Version:
-    def __init__(self, version):
-        self.version = version
+
+    _word_priority = {"a": ALPHA_WEIGHT, "alpha": ALPHA_WEIGHT,
+                      "b": BETA_WEIGHT, "beta": BETA_WEIGHT,
+                      "rc": RELEASE_CANDIDATE_WEIGHT
+                      }
+
+    _version_parser = re.compile(r"[0-9]+|[a-zA-Z]+")
+
+    @staticmethod
+    def _normalize_len(seq1: tuple, seq2: tuple):
+        if len(seq1) == len(seq2):
+            return seq1, seq2
+
+        elif len(seq1) > len(seq2):
+            diff = len(seq1) - len(seq2)
+            seq2_extended = seq2 + (0,) * diff
+            return seq1, seq2_extended
+
+        else:
+            diff = len(seq2) - len(seq1)
+            seq1_extended = seq1 + (0,) * diff
+            return seq1_extended, seq2
+
+    def __init__(self, version: str):
+        self._value = version
+        self._value_tuple = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def value_tuple(self):
+        if self._value_tuple is None:
+            value_list_row = self._version_parser.findall(self.value)
+            for index, value in enumerate(value_list_row):
+                if value.isnumeric():
+                    value_list_row[index] = int(value)
+                else:
+                    value_list_row[index] = self._word_priority[value.lower()]
+
+            self._value_tuple = tuple(value_list_row)
+
+        return self._value_tuple
+
+    def __eq__(self, other):
+        if self.value_tuple == other.value_tuple:
+            return True
+        else:
+            seq1, seq2 = self._normalize_len(self.value_tuple, other.value_tuple)
+            return seq1 == seq2
+
+    def __lt__(self, other):
+        seq1, seq2 = self._normalize_len(self.value_tuple, other.value_tuple)
+        return seq1 < seq2
+
+    def __repr__(self):
+        return f"{type(self).__name__}('{self.value}')"
 
 
 def main():
